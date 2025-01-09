615fa42c48ea3895da656caf1aa5a66d65589e36
wongsakron kongkamud <wongsakronkongkamud@gmail.com>
2025-01-09 14:16:15 +0700

    feat: crate Footer & Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,22 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './components/hoom/Home'
 import EditRoom from './components/room/EditRoom';
 import ExistingRooms from './components/room/ExistingRoom'
+import NavBar from './components/layout/NavBar'
+import Footer from './components/layout/Footer'
 function App() {
   return (
     <>
     <main>
       <Router>
+        <NavBar />
         <Routes>
           <Route path="/" element={<Home/>} />
           <Route path="/edit-room/:roomId" element={<EditRoom />} />
           <Route path="/existing-rooms" element={<ExistingRooms />} />
           <Route path="/add-room" element={<AddRoom />} />
-
         </Routes>
       </Router>
+      <Footer/>
     </main>
       
     </>
