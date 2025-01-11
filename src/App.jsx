7814f093589e81a6b320715de46f98ece8147926
wongsakron kongkamud <wongsakronkongkamud@gmail.com>
2025-01-11 15:29:03 +0700

    feat: browse all room page

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import EditRoom from './components/room/EditRoom';
 import ExistingRooms from './components/room/ExistingRoom'
 import NavBar from './components/layout/NavBar'
 import Footer from './components/layout/Footer'
+import RoomListing from './components/room/RoomListing'
 function App() {
   return (
     <>
@@ -20,6 +21,8 @@ function App() {
           <Route path="/edit-room/:roomId" element={<EditRoom />} />
           <Route path="/existing-rooms" element={<ExistingRooms />} />
           <Route path="/add-room" element={<AddRoom />} />
+          <Route path="//browse-all-rooms" element={<RoomListing />} />
+
         </Routes>
       </Router>
       <Footer/>
